accb5e52a196eb66f3be2749666a9a97e0bb006a
Jacob Wilks <jwilks@mail.csuchico.edu>
2018-09-18 13:01:54 -0700

    We set up Express and Routing endpoints for later use.

--- app.js
+++ app.js
@@ -4,9 +4,36 @@ const bodyParser = require('body-parser');
 const cors = require('cors');
 const passport = require('passport');
 const mongoose = require('mongoose');
+const config = require('./config/database')
+
+
+// Connect to Database
+mongoose.connect(config.database);
+// Once Connected to DB
+mongoose.connection.on('connected', () => {
+    console.log('connected to db, YAY! ' + config.database);
+});
+// If an error connecting to db
+mongoose.connection.on('error', (err) => {
+    console.log('Database error: ' + err);
+});
 
 const app = express();
+const users = require('./routes/users');
+
+//Port Number
 const port = 3000;
+
+app.use(cors());
+
+//Set static folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+//Creating body parser
+app.use(bodyParser.json());
+
+app.use('/users', users);
+
 
 app.get('/', (req, res) => {
     res.send('invalid endpoint');
