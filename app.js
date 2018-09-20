f6badf8e01dee77fe59bb62922b70c38e37e84d9
Nathan Larsen <nlarsen2@mail.csuchico.edu>
2018-09-20 16:44:47 -0700

    added mongoose database model for adding users as well as bcrypt password hashing

--- app.js
+++ app.js
@@ -6,13 +6,15 @@ const passport = require('passport');
 const mongoose = require('mongoose');
 const config = require('./config/database')
 
-
+ 
 // Connect to Database
 mongoose.connect(config.database);
+
 // Once Connected to DB
 mongoose.connection.on('connected', () => {
     console.log('connected to db, YAY! ' + config.database);
 });
+
 // If an error connecting to db
 mongoose.connection.on('error', (err) => {
     console.log('Database error: ' + err);
@@ -21,15 +23,15 @@ mongoose.connection.on('error', (err) => {
 const app = express();
 const users = require('./routes/users');
 
-//Port Number
+// Port Number
 const port = 3000;
 
 app.use(cors());
 
-//Set static folder
+// Set static folder
 app.use(express.static(path.join(__dirname, 'public')));
 
-//Creating body parser
+// Creating body parser
 app.use(bodyParser.json());
 
 app.use('/users', users);
--- routes/users.js
+++ routes/users.js
@@ -1,22 +1,39 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../models/user');
+const passport = require('passport');
+const jwt = require('jsonwebtoken');
 
 
-router.get('/register', (req, res, next) => {
-    res.send('REGISTER');
+
+router.post('/register', (req, res, next) => {
+    let newUser = new User({
+        name: req.body.name,
+        email: req.body.email,
+        username: req.body.username,
+        password: req.body.password
+    });
+
+    User.addUser(newUser, (err, user) => {
+        if(err){
+            res.json({success: false, msg: 'Failed to register user'});
+        } else {
+            res.json({success: true, msg: 'User registered'});
+        }
+    });
 });
 
-//Authenticate Route
-router.get('/authenticate', (req, res, next) => {
+// Authenticate Route
+router.post('/authenticate', (req, res, next) => {
     res.send('AUTHENTICATE');
 });
 
-//Profile
+// Profile
 router.get('/profile', (req, res, next) => {
     res.send('PROFILE');
 });
 
-//Validate
+// Validate
 router.get('/validate', (req, res, next) => {
     res.send('VALIDATE');
 });
