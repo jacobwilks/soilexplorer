fdcc6ece358e91a8e6e17a62b34d7a96d9db4f32
Nathan Larsen <nlarsen2@mail.csuchico.edu>
2018-11-16 15:47:04 -0800

    mlab

--- app.js
+++ app.js
@@ -83,7 +83,7 @@ app.get('*', (req, res) => {
 })
 
 
-server.listen(server_port, server_host, function() {
+app.listen(server_port, server_host, function() {
     console.log('Listening on port %d', server_port);
 });
 // app.listen(port,() => {
