227ee01a3e76bb281b31b26ea1fae5ecb1bb2526
Nathan Larsen <nlarsen2@mail.csuchico.edu>
2018-11-16 15:45:17 -0800

    mlab

--- app.js
+++ app.js
@@ -53,7 +53,7 @@ const users = require('./routes/users');
 
 // Port Number
 // const port = process.env.port || 8080
-var server_port = process.env.YOUR_PORT || process.env.PORT || 80;
+var server_port = process.env.YOUR_PORT || process.env.PORT || 8080;
 var server_host = process.env.YOUR_HOST || '0.0.0.0';
 
 app.use(cors());
