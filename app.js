4973d5c6b2762634a10f5607ddbf19e9f149f59b
Jacob Wilks <jwilks@mail.csuchico.edu>
2018-12-05 22:45:50 -0800

    testing table

--- app.js
+++ app.js
@@ -119,3 +119,16 @@ sequelize
   .then(users => {
       console.log(users)
   })
+
+
+  /*var table = document.createElement('table'), tr, td, row, cell;
+  for (row = 0; row < 10; row++) {
+      tr = document.createElement('tr');
+      for (cell = 0; cell < 22; cell++) {
+          td = document.createElement('td');
+          tr.appendChild(td);
+          td.innerHTML = row * 22 + cell + 1;
+      }
+      table.appendChild(tr);
+  }
+  document.getElementById('container').appendChild(table);*/
