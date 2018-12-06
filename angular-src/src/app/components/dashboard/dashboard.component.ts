3b9f560ee70c788c93800a6ae6aaf041a04bfb9a
Nathan Larsen <nlarsen2@mail.csuchico.edu>
2018-12-06 12:38:06 -0800

    testing

--- angular-src/src/app/components/dashboard/dashboard.component.ts
+++ angular-src/src/app/components/dashboard/dashboard.component.ts
@@ -2,16 +2,8 @@ import { Component, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-dashboard',
-  template: `
-  <div>
-      <label>PDF src</label>
-      <input type="text" placeholder="PDF src" [(ngModel)]="pdfSrc">
-  </div>
-  <pdf-viewer [src]="pdfSrc" 
-              [render-text]="true"
-              style="display: block;"
-  ></pdf-viewer>
-  `
+  templateUrl: './dashboard.component.html',
+  styleUrls: ['./dashboard.component.css']
 })
 
 export class DashboardComponent implements OnInit {
